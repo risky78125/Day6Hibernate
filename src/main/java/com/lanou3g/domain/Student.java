ce90f7b33d2dd6b244b71931b9b4278adc3d15bf
武奇 <risky5200@gmail.com>
2017-10-30 11:58:26 +0800

    增加hibernate配置

---
+++ src/main/java/com/lanou3g/domain/Student.java
@@ -0,0 +1,72 @@
+package com.lanou3g.domain;
+
+/**
+ * Author: 武奇<p/>
+ * Company: lanou3g.com<p/>
+ * Date: 2017/10/30
+ */
+public class Student {
+
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Student(int id, String name, String gender, int age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
